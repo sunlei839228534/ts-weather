fcd3d82561ad0e2ceddc27e290f44818a7c7bcb8
孙磊 <839228534@qq.com>
2020-04-09 11:55:28 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,10 @@ import {createAxios,key} from './api/request'
 import {UserInfoResponse, weatherResponse} from './types/res'
 
 function App () {
-  const [staticMap,setStaticMap] = useState('')
-  useEffect(() => {
-    createAxios<UserInfoResponse>(`v3/ip?key=${key}`).then(res=>{
-      console.log(res)
-    })
-  },[])
-  useEffect(() => {
-    createAxios<string>(`v3/staticmap?location=121.757560,31.144538&zoom=10&key=${key}`).then(res =>{
-      setStaticMap(encodeURI(res.data))
-    })
-  },[])
+
   return (
     <div>
-      <img src={`https://restapi.amap.com/v3/staticmap?location=121.757560,31.144538&zoom=10&key=${key}`} alt=""/>
+      
     </div>
   )
 }
